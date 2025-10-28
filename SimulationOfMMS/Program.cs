96168e1b8ada39ba6d3bb3d4dbec3c5340810b28
Muhammad Huzaifa <muhammadhuzaifa_mh@yahoo.com>
2025-10-28 20:37:37 +0500

    Update Program.cs

--- SimulationOfMMS/Program.cs
+++ SimulationOfMMS/Program.cs
@@ -100,6 +100,7 @@ internal class Program
     static double Factorial(int n)
     {
         double result = 1;
+
         for (int i = 2; i <= n; i++)
             result *= i;
         return result;
